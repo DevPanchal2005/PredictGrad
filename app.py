b05489f13d1a082d415aa1d8727db02fee47848e
Dev Panchal <dev246308@gmail.com>
2026-03-27 10:44:42 +0530

    Integrate Google Fonts for custom styling
    Add custom font styling using Google Fonts to the app.

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+st.markdown("""
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
+    <style>
+        body, .stMarkdown, .stButton button {
+            font-family: 'JetBrains Mono', monospace !important;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Define pages 
 home = st.Page("pages/Welcome.py", icon='🏠')
 
